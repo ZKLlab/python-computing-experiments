4edc9e0623d8070a334a855a8243051ef4ea4466
钟坤林 <zkl@zkllab.com>
2020-05-09 16:20:04 +0800

    feat: beautify output

--- p1.py
+++ p1.py
@@ -2,6 +2,9 @@ import math
 from itertools import count
 
 if __name__ == '__main__':
+    print('+{:-^5}+{:-^22}+{:-^22}+{:-^22}+'.format('', '', '', ''))
+    print('|{:^5}|{:^22}|{:^22}|{:^22}|'.format('k', 'Ramanujan', 'math.pi', 'error'))
+    print('+{:-^5}+{:-^22}+{:-^22}+{:-^22}+'.format('', '', '', ''))
     xs = 2 * 2 ** 0.5 / 9801  # 系数
     _sum = 0.0  # 倒数和
     for k in count():
@@ -9,8 +12,7 @@ if __name__ == '__main__':
         fm = math.factorial(k) ** 4 * 396 ** (4 * k)  # 分母
         diff = xs * fz / fm  # 项
         _sum += diff
+        print('|{:^5d}|{:^22.16f}|{:^22.16f}|{:^22.16f}|'.format(k, 1 / _sum, math.pi, abs(1 / _sum - math.pi)))
         if diff < 1e-15:
             break
-    print('my result : {:.16f}'.format(1 / _sum))
-    print(' math.pi  : {:.16f}'.format(math.pi))
-    print('  error   : {:.16f}'.format(abs(1 / _sum - math.pi)))
+    print('+{:-^5}+{:-^22}+{:-^22}+{:-^22}+'.format('', '', '', ''))
